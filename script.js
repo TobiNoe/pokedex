e11f06115310d6d268caeb751a97360c81f5364c
Tobias Nölle <tobias.noelle@icloud.com>
2023-12-18 19:50:08 +0100

    added funktion loadPokemon() to read Pokemon charmander from api and get a JSON

---
+++ script.js
@@ -0,0 +1,11 @@
+let currentPokemon;
+
+
+async function loadPokemon() {
+    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let response = await fetch(url);
+    currentPokemon = response.json();
+
+    console.log('loaded Pokemon, 'currentPokemon);
+
+}
