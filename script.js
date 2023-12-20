5e76df3c1837c91fabe95acf16bce9e3e76f49a6
Tobias Nölle <tobias.noelle@icloud.com>
2023-12-20 13:18:08 +0100

    change function call renderPokemonOverviewCard() in function loadPokemon()

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ async function loadPokemon(pokemonName) {
     let response = await fetch(url);
     currentPokemon = await response.json();
 
-    console.log('loaded Pokemon', currentPokemon);
+    renderPokemonOverviewCard(currentPokemon['species']['name'], currentPokemon['sprites']['other']['home']['front_default']);
+    
+    /* console.log('loaded Pokemon', currentPokemon['sprites']['other']['home']['front_default']); */
 }
 
 
@@ -26,7 +28,7 @@ async function loadPokemonList() {
     for (let i = 0; i < pokemonList['results'].length; i++) {
         const pokemonName = pokemonList['results'][i]['name'];
         await loadPokemon(pokemonName);
-        renderPokemonOverviewCard(pokemonName);
+        
         /* console.log(pokemonName); */
     }
 }
@@ -37,11 +39,11 @@ function renderPokemonInfo() {
 }
 
 
-function renderPokemonOverviewCard(pokemonName) {
+function renderPokemonOverviewCard(pokemonName, imgURL) {
     document.getElementById('pokemon_overview').innerHTML += /*html*/`
         <div class="col">
             <div class="card bg-primary" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="${imgURL}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h1>${pokemonName}</h1>
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
