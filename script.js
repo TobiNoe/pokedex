ca2882d882f63e86bbc35973e10a3eefb5180884
Tobias Nölle <tobias.noelle@icloud.com>
2023-12-21 11:12:26 +0100

    add a button in renderFunction

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ async function loadPokemon(pokemonName) {
 
 
 async function loadPokemonList() {
-    let url = 'https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0.';
+    let url = 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0.'; //limit from 10000 to 100 sets!!!
     let response2 = await fetch(url);
     pokemonList = await response2.json();
     /* console.log('loaded PokemonList', pokemonList); */
@@ -46,7 +46,8 @@ function renderPokemonOverviewCard(pokemonName, imgURL) {
                 <img src="${imgURL}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h1>${pokemonName}</h1>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Detail Informationen</a>
                 </div>
             </div>
         </div>`
